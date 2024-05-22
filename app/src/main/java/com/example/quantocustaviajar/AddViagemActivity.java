6c4b0b03b34669a9d1bf2c05df45dcf9b2c4ac25
enrico <enricomaine@gmail.com>
2024-05-22 00:08:23 -0300

    Máscara de data

--- app/src/main/java/com/example/quantocustaviajar/AddViagemActivity.java
+++ app/src/main/java/com/example/quantocustaviajar/AddViagemActivity.java
@@ -1,20 +1,27 @@
 package com.example.quantocustaviajar;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.quantocustaviajar.db.Helper;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class AddViagemActivity extends AppCompatActivity {
 
     private EditText etDestino, etTotalViajantes, etDataInicio;
     private Button btnVoltar, btnConfirmAddViagem;
+    private Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,16 @@ public class AddViagemActivity extends AppCompatActivity {
         etDataInicio = findViewById(R.id.etDataInicio);
         btnVoltar = findViewById(R.id.btnVoltar);
         btnConfirmAddViagem = findViewById(R.id.btnConfirmAddViagem);
+        calendar = Calendar.getInstance();
+
+        etDataInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AddViagemActivity.this, dateSetListener,
+                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +67,23 @@ public class AddViagemActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yyyy"; // In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        etDataInicio.setText(sdf.format(calendar.getTime()));
     }
 
     private void addViagem(String destino, String totalViajantes, String dataInicio) {
--- app/src/main/java/com/example/quantocustaviajar/EndViagemActivity.java
+++ app/src/main/java/com/example/quantocustaviajar/EndViagemActivity.java
@@ -1,22 +1,27 @@
 package com.example.quantocustaviajar;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.quantocustaviajar.db.Helper;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class EndViagemActivity extends AppCompatActivity {
 
     private EditText etDataFim;
     private Button btnVoltar, btnEndViagem;
+    private Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,16 @@ public class EndViagemActivity extends AppCompatActivity {
         etDataFim = findViewById(R.id.etDataFim);
         btnVoltar = findViewById(R.id.btnVoltar);
         btnEndViagem = findViewById(R.id.btnEndViagem);
+        calendar = Calendar.getInstance();
+
+        etDataFim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(EndViagemActivity.this, dateSetListener,
+                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +63,23 @@ public class EndViagemActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+
+    private void updateLabel() {
+        String myFormat = "dd/MM/yyyy"; // In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        etDataFim.setText(sdf.format(calendar.getTime()));
     }
 
     private void updateViagemStatus(String dataFim) {
